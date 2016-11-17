9b5f61bc21dfb8ca07297112a920e283124daf2c
akenkao <akenkao@bluetechnology.com.tw>
2016-11-17 09:45:21 +0800

    add tomcat-embed-jasper lib

--- SpringBootStart/src/main/java/com/ns/controller/HelloBootController.java
+++ SpringBootStart/src/main/java/com/ns/controller/HelloBootController.java
@@ -1,15 +1,18 @@
-package com.rl.controller;
+package com.ns.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloBootController {
 	@RequestMapping("/")
-	public String helloPage() {
-		return "Hello! Spring Boot!";
+	public ModelAndView helloPage() {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("name", "ken");
+		return mav;
 	}
 
 	@RequestMapping("/hello/{name}")
--- SpringBootStart/src/main/webapp/WEB-INF/jsp/index.jsp
+++ SpringBootStart/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Welcome!!${hello}
+	Welcome!!${name}
 </body>
 </html>
